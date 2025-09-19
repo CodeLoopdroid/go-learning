6ddada99d8177112b9950522704ef26a666c15f6
Divyesh <divcodings@gmail.com>
2025-09-20 01:08:24 +0530

    Auto commit on Sat 09/20/2025  1:08:23.95

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 import "fmt"
 
 func main(){
-	var conference_name = "Go Conference"
-	const consttickets = 50
-	var remaining_tickets = 50
-
+	var conference_name string = "Go Conference"
+	const consttickets int = 50
+	var remaining_tickets uint = 50 // Explicitly define the datatype
+	
+	fmt.Printf("DataType of %T, DataType of %T, DataType of %T", conference_name, consttickets, remaining_tickets)
 	fmt.Printf("Welcome to %v booking application\n", conference_name)
 	fmt.Printf("We have total of %v tickets and %v are available\n", consttickets, remaining_tickets)
-	fmt.Println("Get your tickets here to attend\n")
+	fmt.Println("Get your tickets here to attend")
+
+	var newname string
+	var userTickets int
+
+	newname = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets\n", newname, userTickets)
 }
