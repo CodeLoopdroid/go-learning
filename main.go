ab69646123cb9bbd3e3c2eb8d5a589ef46f56c9c
Divyesh <divcodings@gmail.com>
2025-09-20 01:28:18 +0530

    Auto commit on Sat 09/20/2025  1:28:18.54

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 import "fmt"
 
 func main(){
-	var conference_name string = "Go Conference"
+	conference_name  := "Go Conference" // You can’t use := outside a function (like at package level). There you must use var.
+	x := 5
+	x, y := 10, 20  // x already exists, but y is new → allowed
+	fmt.Println(x, y)
 	const consttickets int = 50
 	var remaining_tickets uint = 50 // Explicitly define the datatype
 	
@@ -14,7 +17,9 @@ func main(){
 	var newname string
 	var userTickets int
 
-	newname = "Tom"
+	fmt.Scan(newname)
+
+	
 	userTickets = 2
 
 	fmt.Printf("User %v booked %v tickets\n", newname, userTickets)
