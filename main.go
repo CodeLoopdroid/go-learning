8a7dbc7e17a5eb7e7c87a1c824d3df8c2f5002ee
Divyesh <divcodings@gmail.com>
2025-09-20 00:38:09 +0530

    Auto commit on Sat 09/20/2025  0:38:08.99

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Print("Welcome to our conference booking application");
+	var conference_name = "Go Conference"
+	const consttickets = 50
+	var remaining_tickets = 50
+
+	fmt.Println("Welcome to %v booking application", conference_name)
+	fmt.Println("We have total of %v tickets and %v are available", consttickets, remaining_tickets)
 	fmt.Println("Get your tickets here to attend")
-
-	var conference_name = "Go Conference";
-
 }
